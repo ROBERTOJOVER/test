71e14b4dd2f0a685d1cb3853676641ace2fc3e07
ROBERTO JOVER <robertojoverrico@yahoo.es>
2020-11-06 16:45:35 +0100

    Añado ejemplo

---
+++ Ejemplo.cs
@@ -0,0 +1,15 @@
+using System;
+public class Ejercicio3263
+{
+    public static void Main()
+    {
+       
+       
+       
+       {
+           
+           Console.Write("Hello world");
+           Console.WriteLine();
+       }
+     }    
+}
